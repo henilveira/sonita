0f78cb2abd2cf17b297eed6ede9d4ef8c053e8bb
henilveira <henriqueluiz16022000@gmail.com>
2025-02-17 20:25:50 -0300

    feature: utils

--- src/main/java/com/henilveira/sonita/utils/CookieUtil.java
+++ src/main/java/com/henilveira/sonita/utils/CookieUtil.java
@@ -1,20 +1,35 @@
 package com.henilveira.sonita.utils;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CookieUtil {
 
-    public static void addJwtCookie(HttpServletResponse response, String jwtToken) {
-        Cookie cookie = new Cookie("jwt", jwtToken);
-        cookie.setHttpOnly(true); // 🔹 Torna o cookie inacessível via JavaScript
-        cookie.setSecure(true);   // 🔹 Garante que seja enviado apenas em HTTPS
-        cookie.setPath("/");      // 🔹 Disponível em toda a aplicação
-        cookie.setMaxAge(3600);   // 🔹 Expira em 1 hora
-        cookie.setDomain("localhost"); // 🔹 Define o domínio permitido
+    public static void addJwtCookie(HttpServletResponse response, String jwtToken, String tokenName) {
+        Cookie cookie = new Cookie(tokenName, jwtToken);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(3600);
+
 
         response.addCookie(cookie);
+    }
+
+    public static void removeCookie(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().contains("Token")) {
+                    cookie.setValue("");
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                }
+            }
+        }
     }
 }
